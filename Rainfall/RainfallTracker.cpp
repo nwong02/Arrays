c14a76a442fcd04235b97e47aca2b7fe1abe7d8d
Nelson Wong <nelsonwong107@gmail.com>
2021-09-24 23:31:07 -0400

    Hmmm...

--- Rainfall/RainfallTracker.cpp
+++ Rainfall/RainfallTracker.cpp
@@ -150,3 +150,52 @@ void barAsterisk(int stars)
 		cout << "*";
 	}
 }
+
+
+
+
+void displayMonth(int month);
+
+void displayMonth(int month)
+{
+	cout.width(8);
+	switch (month)
+	{
+	case 0:
+		cout << "January";
+		break;
+	case 1:
+		cout << "February";
+		break;
+	case 2:
+		cout << "March";
+		break;
+	case 3:
+		cout << "April";
+		break;
+	case 4:
+		cout << "May";
+		break;
+	case 5:
+		cout << "June";
+		break;
+	case 6:
+		cout << "July";
+		break;
+	case 7:
+		cout << "August";
+		break;
+	case 8:
+		cout << "September";
+		break;
+	case 9:
+		cout << "October";
+		break;
+	case 10:
+		cout << "November";
+		break;
+	case 11:
+		cout << "December";
+		break;
+	}
+}
