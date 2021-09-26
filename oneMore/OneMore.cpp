8f41c9b73f804e218883716d4bc654c24501a33e
Nelson Wong <nelsonwong107@gmail.com>
2021-09-25 22:45:20 -0400

    Added self test exercise 13 (oneMore function)

---
+++ oneMore/OneMore.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Title			: OneMore.cpp
+* Project		: Arrays
+* Author		: Nelson Wong	
+* Description	: From Savitch 4th Ed. Chapter 5 self test exercises
+*/
+
+
+int oneMore(int a[], int entries)
+{
+	for (int i = 0; i < entries; i++)
+	{
+		a[i] =  a[i] + 1;
+		printf("%d", a[i]);
+	}
+	return 1;
+}
+
+
+
+
+
+
+
+
+int main()
+{
+	int values[5] = { 1, 2, 3, 4, 5 };
+	oneMore(values, 5);
+	return 1;
+}
