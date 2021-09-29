a76ed33d4df9c6795f5a6f4b3163c22d6375e1b7
Nelson Wong <nelsonwong107@gmail.com>
2021-09-29 14:41:51 -0400

    Number of entries counter file added

---
+++ EntryCounter/CountEntries.cpp
@@ -0,0 +1,30 @@
+/*
+* Title			: CountEntries.cpp
+* Project		: Arrays - Entry Counter
+* Author		: Nelson Wong
+* Description	: From Savitch Absolute C++ 4th Ed. Chapter 5. Write a program that reads in an array of type int. You may assume that there are fewer than 50 entries in the array. Your program determines how many entries are used and the output is to be a two-column list. The first column is a list of distinct array elements, the second column is a count of the number of occurrences of each element. The list should be sorted on entries in the first column largest to smallest
+*/
+
+
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+#define MAX_ENTRIES 50;
+
+
+int values(int entries[], int numberOfEntries)
+{
+	for (int i = 0; i < numberOfEntries; i++)
+	{
+
+	}
+}
+
+int main()
+{
+	return 1;
+
+}
